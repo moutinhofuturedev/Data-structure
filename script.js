4e12e625c54349b6593cbdd3456731db1600bc32
Paulo Moutinho <paulo_vicali@icloud.com>
2022-08-03 16:47:18 -0300

    implementing concepts inside the array

--- script.js
+++ script.js
@@ -29,3 +29,48 @@ console.log(`Encontramos o piloto ${getPilot}`)
 pilotos.splice(1, 1)
 console.log(pilotos) // => ['Senna', 'Schumacher', 'Hammilton', 'Alonso'] deletou o index [1]
 
+pilotos.pop() // => tira a última posição do array
+console.log(`tirei da lista os pilotos ${pilotos}`)
+
+// STACK NO CÓDIGO
+// Passo 1: modelando
+class Stack {
+  constructor() {
+    this.data = []
+    this.top = -1
+  }
+  push(value) {
+    this.top++
+    this.data[this.top] = value
+
+    return this.data
+  }
+  pop() {
+    if (this.top < 0) return undefined
+
+    const poppedTop = this.data[this.top]
+    delete this.data[this.top]
+
+    this.top--
+    return poppedTop
+  }
+  peek() {
+    return this.top >= 0 ? this.data[this.top] : undefined 
+  }
+}
+
+// Passo 2: Utilizando
+const stack = new Stack()
+
+// Passo 3: adicionar dados
+stack.push('Learning')
+stack.push('data')
+console.log(stack.push('structures'))
+
+console.log(stack.peek())
+
+// --- remover
+stack.pop()
+console.log(stack.pop())
+
+console.log(stack.peek())
